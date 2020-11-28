36944fddf3c0cfbe56595b47bace30e5ed661075
marlon-rt14 <mjavierrt14@mail.com>
2020-11-28 13:35:34 -0500

    Fix: Se eliminaba el item que se deslizaba y el ultimo al mismo tiempo, la lista no se actualizaba al eliminar un item

--- libuy/Helper/SwipeToDelete_v2.cs
+++ libuy/Helper/SwipeToDelete_v2.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Graphics;
 using Android.OS;
 using Android.Runtime;
 using Android.Support.V7.Widget;
@@ -21,11 +22,12 @@ namespace libuy.Helper
         private Context mContext;
         private int position;
 
+
         public SwipeToDelete_v2(RecyclerView.Adapter adapter, Context context)
         {
             mAdapter = (MyAdapter)adapter;
             mContext = context;
-            position = new int();
+            
         }
 
         public override int GetMovementFlags(RecyclerView p0, RecyclerView.ViewHolder p1)
@@ -45,6 +47,12 @@ namespace libuy.Helper
             mAdapter.deleteItem(position);
             RecyclerView vista = ((MainActivity)mContext).Window.DecorView.FindViewById<RecyclerView>(Resource.Id.recycler_view_main);
             vista.ScrollToPosition(position);
+            vista.SetAdapter(mAdapter);
+        }
+
+        public override void OnChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, bool isCurrentlyActive)
+        {
+            base.OnChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
         }
     }
 }
